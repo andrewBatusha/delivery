22733998e1c3553cb2f13672b144d9c4008f96da
andrewBatusha <andrew.batuiev@gmail.com>
2021-02-15 03:23:15 +0200

    finally

--- src/main/java/com/coursework/demo/dto/CallDTO.java
+++ src/main/java/com/coursework/demo/dto/CallDTO.java
@@ -2,6 +2,7 @@ package com.coursework.demo.dto;
 
 import com.coursework.demo.entity.Client;
 import com.coursework.demo.entity.DeliveryMan;
+import com.coursework.demo.entity.Order;
 import com.coursework.demo.entity.enums.CallStatus;
 import lombok.Data;
 
@@ -9,7 +10,7 @@ import lombok.Data;
 public class CallDTO {
     private long id;
 
-    private String order;
+    private Order order;
 
     private DeliveryMan deliveryman;
 
